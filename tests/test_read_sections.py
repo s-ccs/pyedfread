550f6613ca60a6f0a9db978426a432c15c68fefe
Neal Morton <mortonne@gmail.com>
2022-03-20 02:23:36 -0500

    test: reading messages

--- tests/test_read_sections.py
+++ tests/test_read_sections.py
@@ -24,3 +24,22 @@ def test_read_preamble(edf_file):
 ** CAMERA_CONFIG: BBF30200.SCD
 """
     assert preamble == expected
+
+
+def test_read_messages(edf_file):
+    """Read reading messages."""
+    messages = edf.read_messages(edf_file)
+    expected = [
+        'DISPLAY_COORDS 0 0 1919 1079',
+        '',
+        'RECCFG CR 1000 2 1 R',
+        'ELCLCFG MTABLER',
+        'GAZE_COORDS 0.00 0.00 1919.00 1079.00',
+        'THRESHOLDS R 85 220',
+        'ELCL_WINDOW_SIZES 176 188 0 0',
+        'ELCL_PROC CENTROID (3)',
+        'ELCL_PCR_PARAM 5 3.0',
+        'ELCL_PCR_PARAM 5 3.0',
+        '!MODE RECORD CR 1000 2 1 R'
+    ]
+    assert messages[:len(expected)] == expected
